0bc365830929067156de3b1717c33f8f349b3763
purunsingh07 <purunsingh07@gmail.com>
2025-03-15 01:48:26 +0530

    done industry insights

--- actions/user.js
+++ actions/user.js
@@ -2,6 +2,7 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server"
+import { generateAIInsights } from "./dashboard";
 
 export async function updateUser(data){
  const {userId} = await auth();
@@ -29,19 +30,14 @@ export async function updateUser(data){
 
     //dosn,t,create later with ai
       if(!industryInsight){
-        industryInsight=await tx.industryInsight.create({
-            data:{
-                industry: data.industry,
-                salaryRanges:[],
-                growthRate:0,
-                demandLevel:"MEDIUM",
-                topSkills:[],
-                marketOutlook:"NEUTRAL",
-                keyTrends:[],
-                recommendedSkills:[],
-                nextUpdate:new Date(Date.now()+7*24*60*60*1000),
+        const insights = await generateAIInsights(data.industry);
 
-            },
+        industryInsight= await db.industryInsight.create({
+            data:{
+                industry:data.industry,
+                ...insights,
+                nextUpdate:new Date(Date.now()+7*24*60*60*1000)
+            }
         });
       }
     //update the user
